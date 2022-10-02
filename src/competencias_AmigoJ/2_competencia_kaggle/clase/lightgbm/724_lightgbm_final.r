7c9981ced2d8934ba9568d1360cbab0eb3af1016
jesiamigo <jesi.amigo@gmail.com>
2022-10-02 19:09:52 -0300

    prueba con iter 73 de 10 repeated 5foldcv. Mayor ganancia hasta el momento}

--- src/competencias_AmigoJ/2_competencia_kaggle/clase/lightgbm/724_lightgbm_final.r
+++ src/competencias_AmigoJ/2_competencia_kaggle/clase/lightgbm/724_lightgbm_final.r
@@ -16,19 +16,19 @@ require("lightgbm")
 #defino los parametros de la corrida, en una lista, la variable global  PARAM
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
-PARAM$experimento  <- "KA7240_HP_10repeat5foldcv_v1_56"
+PARAM$experimento  <- "KA7240_HP_10repeat5foldcv_v2_73"
 
 PARAM$input$dataset       <- "./datasets/competencia2_2022.csv.gz"
 PARAM$input$training      <- c( 202103 )
 PARAM$input$future        <- c( 202105 )
 
 PARAM$finalmodel$max_bin           <-     31
-PARAM$finalmodel$learning_rate     <-      0.081445993
-PARAM$finalmodel$num_iterations    <-    105
-PARAM$finalmodel$num_leaves        <-  16
-PARAM$finalmodel$min_data_in_leaf  <-   6353
-PARAM$finalmodel$feature_fraction  <-    0.585517471
-PARAM$finalmodel$semilla           <- 954011
+PARAM$finalmodel$learning_rate     <-     0.005161971
+PARAM$finalmodel$num_iterations    <-    919
+PARAM$finalmodel$num_leaves        <-  708
+PARAM$finalmodel$min_data_in_leaf  <-   91
+PARAM$finalmodel$feature_fraction  <-    0.525727348
+PARAM$finalmodel$semilla           <- 124769
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
