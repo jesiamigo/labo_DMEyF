62344bc2a38b4d1b772da9f84932a90eeacde371
jesiamigo <jesi.amigo@gmail.com>
2022-10-28 17:30:46 -0300

    prueba con mas envios - v1

--- src/competencias_AmigoJ/3_competencia_kaggle/v1/z991_ZZ_lightgbm.r
+++ src/competencias_AmigoJ/3_competencia_kaggle/v1/z991_ZZ_lightgbm.r
@@ -138,8 +138,8 @@ for( i in  1:PARAM$modelos )
 
 
   #genero los archivos para Kaggle
-  cortes  <- seq( from=  7000,
-                  to=   11000,
+  cortes  <- seq( from=  11500,
+                  to=   15000,
                   by=     500 )
 
 
